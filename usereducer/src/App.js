eb5bf4e34bb65df0cafcca376d8deccf459b70bd
Kohta9521 <kohta0421kochi@gmail.com>
2023-10-07 22:08:12 +0900

    add redux tutorial is finishes

--- usereducer/src/App.js
+++ usereducer/src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
 import FetchApi from "./components/FetchApi";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
 
 function App() {
   const counter = useSelector((state) => state.counter);
+  const isLogin = useSelector((state) => state.isLogin);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <h1>Hello Redux</h1>
       <h1>Count: {counter}</h1>
+      <button onClick={() => dispatch(increment)}>+</button>
+      {isLogin ? <h3>ログインに成功！</h3> : <h3>ゴグインしてください！</h3>}
     </div>
   );
 }
