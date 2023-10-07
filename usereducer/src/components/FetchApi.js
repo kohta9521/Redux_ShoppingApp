c974187b8ddad2212900269f6aa393c9f6e7d1ab
Kohta9521 <kohta0421kochi@gmail.com>
2023-10-07 17:15:31 +0900

    add simple and sample json logic

---
+++ usereducer/src/components/FetchApi.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const FetchApi = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [todo, setTodo] = useState({});
+
+  const fetchTodos = () => {
+    setLoading(true);
+    setError(false);
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((res) => res.json())
+      .then((data) => {
+        setLoading(false);
+        setTodo(data);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(true);
+      });
+  };
+  return (
+    <>
+      <div>
+        <button onClick={fetchTodos}>{loading ? "処理中" : "クリック"}</button>
+        <p>{todo?.title}</p>
+        <p>{todo?.userId}</p>
+        <span>{error && "エラーです"}</span>
+      </div>
+    </>
+  );
+};
+
+export default FetchApi;
